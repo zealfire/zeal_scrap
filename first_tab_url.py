3a3643c6c0ad386794dd4c116929c7bd7d612d44
zealfire <iamalok185@gmail.com>
2016-06-15 12:23:35 +0530

    starting script for displaying current tab's url added.

---
+++ first_tab_url.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+import sqlite3
+import os
+import sys
+import webbrowser as wb
+#import urllib2
+
+for path in (
+        os.path.expanduser('~/.config/google-chrome/'),
+        os.path.expanduser('~/.config/chromium/'),
+        'C:\\Users\\%s\\AppData\\Local\\Google\\Chrome\\' % os.environ.get('USERNAME'),
+        'C:\\Documents and Settings\\%s\\Local Settings\\Application Data\\Google\\Chrome\\' % os.environ.get('USERNAME')
+        ):
+    path += os.path.join('Default', 'Current Tabs')
+    print (path)
+    if os.path.exists(path):
+        print (path)
+        break
+else:
+    print 'Chrome history file not found!'
+    sys.exit()
+
+conn = sqlite3.connect(path)
+c = conn.cursor()
+
+while True:
+    # c.execute('select * from "Current Tabs";')
+    # names = list(map(lambda x: x[0], c.description))
+    # print (names)
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    print(c.fetchall())
+    # url = c.fetchone()[1]
+
+    # try:
+    #     urllib2.urlopen(url).read()
+    # except urllib2.URLError:
+    #     pass
+    # else:
+    #     wb.open(url)
+    break
